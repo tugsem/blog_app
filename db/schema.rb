82e9c48a5518b5cc3308c0c251d3878dbba18cb9
tugsem <saranlitugsem@gmail.com>
2022-08-19 18:57:51 +0300

    Add rspec gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_16_153343) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_18_130515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_153343) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
+    t.string "author"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -41,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_16_153343) do
     t.bigint "user_id", null: false
     t.integer "commentsCounter"
     t.integer "likesCounter"
+    t.string "author"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
