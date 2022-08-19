ac0e3d651b3f3a3ca6498df6b248176f744d7718
tugsem <saranlitugsem@gmail.com>
2022-08-19 18:55:56 +0300

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
-  has_many :likes, foreign_key: 'user_id'
-  has_many :comments, foreign_key: user_id
-  has_many :posts, foreign_key: user_id
+  has_many :likes
+  has_many :comments
+  has_many :posts
+  validates :name, presence: true
+  validates :postsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def return_recent_posts
     posts.order(created_at: :desc).limit(3)
