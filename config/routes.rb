7d0d7cc24f7f8e846beb35366b1e61c72873ae5d
tugsem <saranlitugsem@gmail.com>
2022-08-27 20:34:06 +0300

    Add new resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users do
+    resources :posts do
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
   end
+  get '/users/:user_id/posts/new', :to => 'posts#new'
+  post './users/:user_id/posts', :to => 'posts#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 end
