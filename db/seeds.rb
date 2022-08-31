67c9212b7414ce507fa0d4ccf3cf0d111cd47fe4
tugsem <saranlitugsem@gmail.com>
2022-08-31 19:51:59 +0300

    Add posts and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-5.times do |i|
-  User.create(name: "#{i+1}.User", picture: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png", bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia felis at quam dignissim, id rutrum mi volutpat. Donec ornare elit ante, ac accumsan odio aliquam non. Vestibulum augue ex, auctor nec hendrerit et, venenatis eget ante. Curabitur maximus, odio in pretium finibus, sapien eros mollis massa, sed sodales lorem tellus ac metus. Suspendisse sodales fringilla arcu, eget cursus purus maximus at. Aliquam eget felis non nulla placerat dapibus sit amet vel eros. Curabitur ac mi et ante bibendum maximus. Vivamus ac tempor augue, vel posuere nisi. Fusce in dictum orci. Etiam dignissim libero sit amet sem dictum scelerisque in eu purus.")
-end
