63fb40755ff3b9b12f6305bbb0020aadcd2e1efa
tugsem <saranlitugsem@gmail.com>
2022-09-01 20:11:55 +0300

    Add role column to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  attr_accessible :name, :email
+
+  Roles = [ :admin, :default]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,6 +11,10 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy, foreign_key: :user_id
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def is?( requested_role)
+    self.role == requested_role.to_s
+  end
 
   def return_recent_posts
     posts.order(created_at: :desc).limit(3)
