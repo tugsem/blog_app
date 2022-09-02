42cf8bf231b7a885b58e99f99a08cfea738be195
bizip <bizip04@gmail.com>
2022-09-02 17:10:45 +0200

    Fix applicaion controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,24 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :authenticate_user!
 
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
+  protected
+  # Catch all CanCan errors and alert the user of the exception
+  rescue_from CanCan::AccessDenied do | exception |
+    redirect_to root_url, alert: exception.message
+  end
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :role) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :current_password, :role)
+    end
+  end
+  def after_sign_in_path_for(resource)
+    session["user_return_to"] || root_path
+  end
+
   protected
 
   def configure_permitted_parameters
