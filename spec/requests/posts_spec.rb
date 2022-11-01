e2cb85ab6088c1ec86f9aae19fe07f4f1d3c27fc
tugsem <saranlitugsem@gmail.com>
2022-11-01 15:28:59 +0300

    Add feature tests for users/index page

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before(:each) do
+    @user = User.create(name: "first user", picture: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png", bio: "Lorem ipsum dolor sit amet.")
+    @post = Post.create(title: 'new post', text: 'Lorem ipsum dolor sit amet.', user: @user)
+  end
   describe 'GET /index' do
     it 'works! (response status correct)' do
-      get user_posts_path(1) do
-        response = user_posts_path(1)
+      get user_posts_path(@user) do
+        response = user_posts_path(@user)
         expect(response).to be_successful
       end
     end
 
     it 'works! (renders true template)' do
-      get user_posts_path(1) do
-        response = user_posts_path(1)
+      get user_posts_path(@user) do
+        response = user_posts_path(@user)
         expect(response).to render_template(:index)
       end
     end
 
     it 'works! (response body includes correct text)' do
-      get user_posts_path(1) do
-        response = user_posts_path(1)
+      get user_posts_path(@user) do
+        response = user_posts_path(@user)
         expect(response.body.include?('All posts for a given user')).to be_true
       end
     end
@@ -26,23 +30,23 @@ RSpec.describe 'Posts', type: :request do
 
   describe 'GET /show' do
     it 'works! (response status correct)' do
-      get user_posts_path(1, 1) do
-        response = user_posts_path(1, 1)
+      get user_posts_path(@user, @post) do
+        response = user_posts_path(@user, @post)
         expect(response).to be_successful
       end
     end
 
     it 'works! (renders true template)' do
-      get user_posts_path(1, 1) do
-        response = user_posts_path(1, 1)
+      get user_posts_path(@user, @post) do
+        response = user_posts_path(@user, @post)
         expect(response).to render_template(:show)
       end
     end
 
     it 'works! (now write some real specs)' do
-      get user_posts_path(1, 1) do
-        response = user_posts_path(1, 1)
-        expect(response.body.incluede?('Here is a detailed look for a given post')).to be_true
+      get user_posts_path(@user, @post) do
+        response = user_posts_path(@user, @post)
+        expect(response.body.include?('Here is a detailed look for a given post')).to be_true
       end
     end
   end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before(:each) do
+    @user = User.create(name: "first user", picture: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png", bio: "Lorem ipsum dolor sit amet.")
+  end
   describe 'GET /index' do
     it 'works! (response status correct)' do
       get users_path do
@@ -26,23 +29,23 @@ RSpec.describe 'Users', type: :request do
 
   describe 'GET /show' do
     it 'works! (response status correct)' do
-      get user_path(1) do
-        response = user_path(1)
+      get user_path(@user) do
+        response = user_path(@user)
         expect(response).to be_successful
       end
     end
 
     it 'works! (renders true template)' do
-      get user_path(1) do
-        response = user_path(1)
+      get user_path(@user) do
+        response = user_path(@user)
         expect(response).to render_template(:show)
       end
     end
 
     it 'works! (now write some real specs)' do
-      get user_path(1) do
-        response = user_path(1)
-        expect(response.body.incluede?('More information for a given user')).to be_true
+      get user_path(@user) do
+        response = user_path(@user)
+        expect(response.body.include?('More information for a given user')).to be_true
       end
     end
   end
