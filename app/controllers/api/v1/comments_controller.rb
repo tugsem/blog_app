f287f55a2b1c7b58c3ed360a6224c60c16ddcc80
tkz96 <tkz9696@gmail.com>
2022-11-05 00:25:17 -0700

    Add comments controller module for api

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class CommentsController < ApplicationController
+      def create
+        @user = User.find(params[:user_id])
+        @post = Post.find(params[:post_id])
+        @comment = Comment.new(comment_params)
+        @comment.user_id = @user.id
+        @comment.post_id = @post.id
+
+        if @comment.save
+          render json: @comment, status: :created
+        else
+          render json: comment.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def comment_params
+        params.require(:comment).permit(:text)
+      end
+    end
+
+  end
+end
